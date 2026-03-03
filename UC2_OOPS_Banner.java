327fdaaca1c178eee90dbbcc9ff1de34fcbf5f49
hemanth <hemanthreddy11107@gmail.com>
2026-03-03 16:37:39 +0530

    feature UC2

---
+++ UC2_OOPS_Banner.java
@@ -0,0 +1,20 @@
+public class UC2_OOPS_Banner {
+
+    public static void main(String[] args) {
+
+        System.out.println("First message: Hello, World!");
+        System.out.println("Second message: The result is: 42");
+        System.out.println("Third message: Error: File not found.");
+        System.out.println();
+
+        // OOPS ASCII Banner (7 Lines)
+
+        System.out.println("  ******     ******     ********    ******  ");
+        System.out.println(" **    **   **    **    **     **  **    ** ");
+        System.out.println(" **    **   **    **    **     **  **       ");
+        System.out.println(" **    **   **    **    ********    ******  ");
+        System.out.println(" **    **   **    **    **               ** ");
+        System.out.println(" **    **   **    **    **        **     ** ");
+        System.out.println("  ******     ******     **         ******  ");
+    }
+}
